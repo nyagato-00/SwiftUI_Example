b4b91a550b90496661ce585a898accc7f3932c31
宮原崇志 <nyagato@miyaharatakashinoMacBook-Air.local>
2020-12-29 16:32:34 +0900

    [Change] Use the progress view to show the percentage of elapsed time during the scrum.
    Replace the contents of the existing body with ProgressView, initializing the view with placeholder data.
    
    You’ll use the progress view to display the percentage of time elapsed during the scrum. Progress views can also show indeterminate progress, like when loading a screen.

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ProgressView(value: 5, total: 15)
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        Group {
+            MeetingView()
+        }
     }
 }
