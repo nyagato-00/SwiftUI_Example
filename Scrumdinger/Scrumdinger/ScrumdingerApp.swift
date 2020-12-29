bf8aec642211a9e13ff827f3ebb991669d40dedb
宮原崇志 <nyagato@miyaharatakashinoMacBook-Air.local>
2020-12-29 16:20:54 +0900

    [Add] Create a new Scrumdinger project.
    Interface: SwiftUI
    Life cycle: SwiftUI App

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 宮原崇志 on 2020/12/29.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
